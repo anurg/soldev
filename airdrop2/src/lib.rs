7184c0dbcc780e2be0f4449d6e0f343621135616
anunymous <anurg@yahoo.com>
2025-10-12 16:58:14 +0000

    ...

--- airdrop2/src/lib.rs
+++ airdrop2/src/lib.rs
@@ -14,10 +14,34 @@ mod tests {
     }
 
     #[test]
-    fn base58_to_wallet() {}
+    fn base58_to_wallet() {
+        println!("Input your wallet as base58 string!");
+        let stdin = io::stdin();
+        let base58 = stdin.lock().lines().next().unwrap().unwrap();
+        println!("Your wallet file format is:");
+        let wallet = bs58::decode(base58).into_vec().unwrap();
+        println!("{:?}", wallet);
+    }
 
     #[test]
-    fn wallet_to_base58() {}
+    fn wallet_to_base58() {
+        println!("Input your private key as a JSON byte array (e.g. [12,34,...]):");
+        let stdin = io::stdin();
+        let wallet = stdin
+            .lock()
+            .lines()
+            .next()
+            .unwrap()
+            .unwrap()
+            .trim_start_matches('[')
+            .trim_end_matches(']')
+            .split(',')
+            .map(|s| s.trim().parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+        println!("Your Base58-encoded private key is:");
+        let base58 = bs58::encode(wallet).into_string();
+        println!("{:?}", base58);
+    }
 
     #[test]
     fn claim_airdrop() {}
