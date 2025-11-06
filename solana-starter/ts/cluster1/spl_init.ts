a7837ca7aa7a5e9673920915675f5beaea6fff70
anurg <anurg@yahoo.com>
2025-11-06 07:29:41 +0000

    tone minting

---
+++ solana-starter/ts/cluster1/spl_init.ts
@@ -0,0 +1,27 @@
+import { Connection, Keypair } from "@solana/web3.js";
+import { createMint, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+import wallet from "/home/nkb/.config/solana/id.json";
+
+const keypair = Keypair.fromSecretKey(Uint8Array.from(wallet));
+console.log(`${keypair.publicKey}`);
+(async () => {
+  const connection: Connection = new Connection(
+    "http://localhost:8899",
+    "confirmed"
+  );
+  try {
+    const mint = await createMint(
+      connection,
+      keypair,
+      keypair.publicKey,
+      null,
+      6,
+      undefined,
+      {},
+      TOKEN_2022_PROGRAM_ID
+    );
+    console.log(`Token Minted- ${mint}`);
+  } catch (e) {
+    console.log(`Some Error - ${e}`);
+  }
+})();
