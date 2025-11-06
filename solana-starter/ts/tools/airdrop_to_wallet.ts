3b28b6f9db256d5a71b40c1cdc6fda3b38ceadde
anurg <anurg@yahoo.com>
2025-11-06 07:08:49 +0000

    practiced base58 to wallet & vice-versa with json file storage.

--- solana-starter/ts/tools/airdrop_to_wallet.ts
+++ solana-starter/ts/tools/airdrop_to_wallet.ts
@@ -1,34 +1,20 @@
-import {
-  Keypair,
-  Connection,
-  type Commitment,
-  LAMPORTS_PER_SOL,
-  PublicKey,
-} from "@solana/web3.js";
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import prompt from "prompt";
+const connection = new Connection("http://localhost:8899", "confirmed");
+prompt.start();
+console.log(`Please enter the wallet address & no of Sol required as airdrop`);
 
 (async () => {
-  const commitment: Commitment = "confirmed";
-  const conn = new Connection("http://localhost:8899");
-
-  //   const keypair = Keypair.generate();
-  prompt.start();
-  console.log("Please enter the wallet address and no of SOL you want");
-
-  const { wallet, noSol } = await prompt.get(["wallet", "noSol"]);
-  console.log(`wallet -${wallet}`);
-  const keypair = new PublicKey(wallet as string);
-  console.log(`keypair -${keypair}`);
   try {
-    // const tx = await conn.requestAirdrop(
-    //   keypair,
-    //   LAMPORTS_PER_SOL * parseInt(noSol as string)
-    // );
-    // console.log(`The txn signature is -${tx}`);
+    const { wallet, noSol } = await prompt.get(["wallet", "noSol"]);
+    const keypair = new PublicKey(wallet as string);
+    const tx = await connection.requestAirdrop(
+      keypair,
+      LAMPORTS_PER_SOL * parseInt(noSol as string)
+    );
 
-    const balance = await conn.getBalance(keypair);
-    console.log(`Balance of wallet is : ${balance}`);
-  } catch (err) {
-    console.log(`Some error-${err}`);
+    console.log(`Transaction signature is : ${tx}`);
+  } catch (e) {
+    console.log(`Some error: ${e}`);
   }
 })();
--- solana-starter/ts/tools/wallet_to_base58.ts
+++ solana-starter/ts/tools/wallet_to_base58.ts
@@ -1,12 +1,21 @@
 import prompt from "prompt";
 import bs58 from "bs58";
+import * as fs from "fs";
+import path from "path";
 
-prompt.start();
-console.log(`Enter the wallet file`);
+const file = path.join(__dirname, "wallet.json");
 
-const { privKey } = await prompt.get(["privKey"]);
-const json = bs58.encode(Buffer.from(JSON.parse(privKey as string)));
-console.log(`${json}`);
+(async () => {
+  try {
+    // prompt.start();
+    // console.log(`Enter the Wallet Byte Array from ~/.config/solana/id.json`);
+    // const { wallet1 } = await prompt.get(["wallet"]);
+    //read from wallet file
+    const wallet = fs.readFileSync(file, "utf-8");
 
-const wallet = bs58.decode(json);
-console.log(`Wallet - ${wallet}`);
+    const keypair = bs58.encode(Buffer.from(JSON.parse(wallet as string)));
+    console.log(`keypair - ${keypair}`);
+  } catch (e) {
+    console.log(`Some error- ${e}`);
+  }
+})();
