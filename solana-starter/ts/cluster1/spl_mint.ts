73e594ae38604840816993a9ec202a60cf185bca
anurg <anurg@yahoo.com>
2025-11-06 09:39:23 +0000

    Spl mint & balance functions

---
+++ solana-starter/ts/cluster1/spl_mint.ts
@@ -0,0 +1,62 @@
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_2022_PROGRAM_ID,
+  getAccount,
+  getMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+} from "@solana/spl-token";
+import wallet from "/home/nkb/.config/solana/id.json";
+import airdrop_to_wallet from "../tools/airdrop_to_wallet";
+
+const keypair = Keypair.fromSecretKey(Uint8Array.from(wallet));
+const mint = new PublicKey("GWCfpQ9TLxNk4rCwpFsGzvKCsvNLWKZwgBRwvdWHjsd3");
+const connection = new Connection("http://localhost:8899", "confirmed");
+const user = Keypair.generate();
+console.log(`User wallet- ${user.publicKey}`);
+
+airdrop_to_wallet(user.publicKey, 100);
+
+(async () => {
+  try {
+    const decimals = (
+      await getMint(connection, mint, "confirmed", TOKEN_2022_PROGRAM_ID)
+    ).decimals;
+    console.log(`decimals - ${decimals}`);
+    const ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      user.publicKey,
+      false,
+      undefined,
+      { commitment: "confirmed" },
+      TOKEN_2022_PROGRAM_ID,
+      ASSOCIATED_TOKEN_PROGRAM_ID
+    );
+    const tx = await mintTo(
+      connection,
+      keypair,
+      mint,
+      ata.address,
+      keypair,
+      1231 * 10 ** decimals,
+      [],
+      { commitment: "confirmed" },
+      TOKEN_2022_PROGRAM_ID
+    );
+    console.log(`Token Minted- ${tx}`);
+    // Check the balance
+    const info = await getAccount(
+      connection,
+      ata.address,
+      "confirmed",
+      TOKEN_2022_PROGRAM_ID
+    );
+    const balance = Number(info.amount) / 10 ** decimals;
+    console.log(`SPL Token Balance- ${balance}`);
+  } catch (e) {
+    console.log(`Some error-${e}`);
+  }
+})();
--- solana-starter/ts/tools/airdrop_to_wallet.ts
+++ solana-starter/ts/tools/airdrop_to_wallet.ts
@@ -1,20 +1,24 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import prompt from "prompt";
 const connection = new Connection("http://localhost:8899", "confirmed");
-prompt.start();
-console.log(`Please enter the wallet address & no of Sol required as airdrop`);
+// prompt.start();
+// console.log(`Please enter the wallet address & no of Sol required as airdrop`);
 
-(async () => {
+export default async function airdrop_to_wallet(
+  wallet: PublicKey,
+  noSol: number
+) {
   try {
-    const { wallet, noSol } = await prompt.get(["wallet", "noSol"]);
-    const keypair = new PublicKey(wallet as string);
+    // const { wallet, noSol } = await prompt.get(["wallet", "noSol"]);
+    // const keypair = new PublicKey(wallet as string);
     const tx = await connection.requestAirdrop(
-      keypair,
-      LAMPORTS_PER_SOL * parseInt(noSol as string)
+      wallet,
+      // LAMPORTS_PER_SOL * parseInt(noSol as string)
+      LAMPORTS_PER_SOL * noSol
     );
 
     console.log(`Transaction signature is : ${tx}`);
   } catch (e) {
     console.log(`Some error: ${e}`);
   }
-})();
+}
