413214e7c8851d55c73c9af46dcb6bd50d71cbe0
anurg <anurg@yahoo.com>
2025-11-07 09:52:38 +0000

    .....

---
+++ marketplace/programs/marketplace/src/state/marketplace.rs
@@ -0,0 +1,11 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(InitSpace)]
+pub struct Marketplace {
+    pub admin: Pubkey,
+    pub fee: u16,
+    pub bump: u8,
+    #[max_len(32)]
+    pub name: String,
+}
