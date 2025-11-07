2d5439671a2ff02ed60e38dbd3e9e17e4f409219
anurg <anurg@yahoo.com>
2025-11-07 07:17:51 +0000

    Added marketplace project

---
+++ marketplace/programs/marketplace/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("6y9TCgoh6u9RnQN7hvy1rEjrjYHdY3X7H6CBBW5iMTLs");
+
+#[program]
+pub mod marketplace {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
