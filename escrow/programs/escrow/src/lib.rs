39cabb5ad1a2b717c3eeb640273de1ed37f6446b
anurg <anurg@yahoo.com>
2025-11-12 11:59:01 +0000

    make instruction completed.

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -15,7 +15,17 @@ declare_id!("2wEGsaYr6qRVG1MBkbATSHeJ7EMmxNVUV8mf2JVSc2hg");
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Make>) -> Result<()> {
+    pub fn make(
+        ctx: Context<Make>,
+        seed: u64,
+        mint_a: Pubkey,
+        mint_b: Pubkey,
+        receive: u64,
+        amount: u64,
+    ) -> Result<()> {
+        ctx.accounts
+            .init_escrow(seed, mint_a, mint_b, receive, ctx.bumps)?;
+        ctx.accounts.deposit(amount)?;
         Ok(())
     }
 }
